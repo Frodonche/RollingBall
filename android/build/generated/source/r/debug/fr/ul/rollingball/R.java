ead82489c30239d6183bd98fd505c659802cb949
Guillaume ROTH <guillaume.roth7@etu.univ-lorraine.fr>
2017-01-29 03:50:40 +0100

    Enfin l'icone pour l'appli

--- android/build/generated/source/r/debug/fr/ul/rollingball/R.java
+++ android/build/generated/source/r/debug/fr/ul/rollingball/R.java
@@ -11,12 +11,18 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_action_name=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int ic_stat_name=0x7f020002;
+        public static final int ouais=0x7f020003;
+    }
+    public static final class mipmap {
+        public static final int ic_launcher=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f030000;
+        public static final int app_name=0x7f040000;
     }
     public static final class style {
-        public static final int GdxTheme=0x7f040000;
+        public static final int GdxTheme=0x7f050000;
     }
 }
